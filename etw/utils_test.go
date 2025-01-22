10b8c860db85d1fa0518fcdec90488aca56f255a
tekert <tekert@gmail.com>
2025-01-22 12:55:29 -0300

    Add missing swap tests

--- etw/utils_test.go
+++ etw/utils_test.go
@@ -119,3 +119,77 @@ func TestSIDConversion(t *testing.T) {
 		})
 	}
 }
+
+// From the custom parser
+func TestSwapFunctions(t *testing.T) {
+	tt := toast.FromT(t)
+
+	// Test SwapBytes
+	t.Run("SwapBytes", func(t *testing.T) {
+
+		// Even length
+		even := []byte{0x12, 0x34, 0x56, 0x78}
+		SwapBytes(even)
+		tt.Assert(even[0] == 0x78 && even[1] == 0x56 &&
+			even[2] == 0x34 && even[3] == 0x12)
+
+		// Odd length
+		odd := []byte{0x12, 0x34, 0x56}
+		SwapBytes(odd)
+		tt.Assert(odd[0] == 0x56 && odd[1] == 0x34 && odd[2] == 0x12)
+
+		// Single byte (should not change)
+		single := []byte{0x12}
+		SwapBytes(single)
+		tt.Assert(single[0] == 0x12)
+
+		// Empty slice (should not panic)
+		empty := []byte{}
+		SwapBytes(empty)
+	})
+
+	// Test Swap16
+	t.Run("Swap16", func(t *testing.T) {
+		tt := toast.FromT(t)
+
+		// Test values
+		tt.Assert(Swap16(0x1234) == 0x3412)
+		tt.Assert(Swap16(0xFF00) == 0x00FF)
+		tt.Assert(Swap16(0x0000) == 0x0000)
+		tt.Assert(Swap16(0xFFFF) == 0xFFFF)
+
+		// Test idempotency
+		n := uint16(0x1234)
+		tt.Assert(Swap16(Swap16(n)) == n)
+	})
+
+	// Test Swap32
+	t.Run("Swap32", func(t *testing.T) {
+		tt := toast.FromT(t)
+
+		// Test values
+		tt.Assert(Swap32(0x12345678) == 0x78563412)
+		tt.Assert(Swap32(0xFF000000) == 0x000000FF)
+		tt.Assert(Swap32(0x00000000) == 0x00000000)
+		tt.Assert(Swap32(0xFFFFFFFF) == 0xFFFFFFFF)
+
+		// Test idempotency
+		n := uint32(0x12345678)
+		tt.Assert(Swap32(Swap32(n)) == n)
+	})
+
+	// Test Swap64
+	t.Run("Swap64", func(t *testing.T) {
+		tt := toast.FromT(t)
+
+		// Test values
+		tt.Assert(Swap64(0x1234567890ABCDEF) == 0xEFCDAB9078563412)
+		tt.Assert(Swap64(0xFF00000000000000) == 0x00000000000000FF)
+		tt.Assert(Swap64(0x0000000000000000) == 0x0000000000000000)
+		tt.Assert(Swap64(0xFFFFFFFFFFFFFFFF) == 0xFFFFFFFFFFFFFFFF)
+
+		// Test idempotency
+		n := uint64(0x1234567890ABCDEF)
+		tt.Assert(Swap64(Swap64(n)) == n)
+	})
+}
