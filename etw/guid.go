dbe2052bebf3f3758f22ad8e53728a2f83d5408b
tekert <tekert@gmail.com>
2025-01-22 12:57:40 -0300

    Add lowercase String representation for GUIDs like microsoft does.

--- etw/guid.go
+++ etw/guid.go
@@ -42,6 +42,7 @@ func (g *GUID) IsZero() bool {
 	return g.Equals(&nullGUID)
 }
 
+// uppercase string representation of the GUID
 func (g *GUID) String() string {
 	return fmt.Sprintf("{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
 		g.Data1,
@@ -49,6 +50,16 @@ func (g *GUID) String() string {
 		g.Data3,
 		g.Data4[0], g.Data4[1],
 		g.Data4[2], g.Data4[3], g.Data4[4], g.Data4[5], g.Data4[6], g.Data4[7])
+}
+
+// lowercase string representation of the GUID
+func (g *GUID) StringL() string {
+    return fmt.Sprintf("{%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x}",
+        g.Data1,
+        g.Data2,
+        g.Data3,
+        g.Data4[0], g.Data4[1],
+        g.Data4[2], g.Data4[3], g.Data4[4], g.Data4[5], g.Data4[6], g.Data4[7])
 }
 
 func (g *GUID) Equals(other *GUID) bool {
